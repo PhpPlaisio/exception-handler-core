f89521cff66ccfed9db21fdf8e0dc4faa90bbb22
Paul Water <p.r.water@setbased.nl>
2020-04-13 00:50:28 +0200

    Reference obfuscator command is a Plaisio command.

--- src/Command/GenerateExceptionHandlerCommand.php
+++ src/Command/GenerateExceptionHandlerCommand.php
@@ -3,13 +3,11 @@ declare(strict_types=1);
 
 namespace Plaisio\ExceptionHandler\Command;
 
-use Composer\IO\ConsoleIO;
-use Plaisio\Console\Style\PlaisioStyle;
+use Plaisio\Console\Command\PlaisioCommand;
+use Plaisio\Console\Helper\TwoPhaseWrite;
 use Plaisio\ExceptionHandler\Helper\ExceptionHandlerCodeGenerator;
 use Plaisio\ExceptionHandler\Helper\ExceptionHandlerMetadataExtractor;
 use Plaisio\ExceptionHandler\Helper\PlaisioXmlHelper;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,23 +15,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 /**
  * Command for generation the code for the core's exception handler.
  */
-class GenerateExceptionHandlerCommand extends Command
+class GenerateExceptionHandlerCommand extends PlaisioCommand
 {
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * The Console IO object.
-   *
-   * @var ConsoleIO
-   */
-  private $consoleIo;
-
-  /**
-   * The output decorator.
-   *
-   * @var PlaisioStyle
-   */
-  private $io;
-
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * @inheritdoc
@@ -51,8 +34,7 @@ class GenerateExceptionHandlerCommand extends Command
    */
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $this->io        = new PlaisioStyle($input, $output);
-    $this->consoleIo = new ConsoleIO($input, $output, $this->getHelperSet());
+    $this->io->title('Plaisio: Generate Core Exception Handler');
 
     $metadataExtractor = new ExceptionHandlerMetadataExtractor($this->io, $input->getArgument('config file'));
     $handlers          = $metadataExtractor->extractExceptionAgents();
@@ -63,48 +45,10 @@ class GenerateExceptionHandlerCommand extends Command
     $generator = new ExceptionHandlerCodeGenerator();
     $code      = $generator->generateCode($class, $handlers);
 
-    $this->writeTwoPhases($path, $code);
+    $writer = new TwoPhaseWrite($this->io);
+    $writer->write($path, $code);
 
     return 0;
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
-   * Writes a file in two phase to the filesystem.
-   *
-   * First write the data to a temporary file (in the same directory) and than renames the temporary file. If the file
-   * already exists and its content is equal to the data that must be written no action  is taken. This has the
-   * following advantages:
-   * <ul>
-   * <li> In case of some write error (e.g. disk full) the original file is kept in tact and no file with partially data
-   *      is written.
-   * <li> Renaming a file is atomic. So, running processes will never read a partially written data.
-   * </ul>
-   *
-   * @param string $filename The name of the file were the data must be stored.
-   * @param string $data     The data that must be written.
-   */
-  private function writeTwoPhases(string $filename, string $data): void
-  {
-    $write_flag = true;
-    if (file_exists($filename))
-    {
-      $old_data = file_get_contents($filename);
-      if ($data==$old_data) $write_flag = false;
-    }
-
-    if ($write_flag)
-    {
-      $tmp_filename = $filename.'.tmp';
-      file_put_contents($tmp_filename, $data);
-      rename($tmp_filename, $filename);
-
-      $this->io->writeln(sprintf('Wrote <fso>%s</fso>', OutputFormatter::escape($filename)));
-    }
-    else
-    {
-      $this->io->writeln(sprintf('File <fso>%s</fso> is up to date', OutputFormatter::escape($filename)));
-    }
   }
 
   //--------------------------------------------------------------------------------------------------------------------
