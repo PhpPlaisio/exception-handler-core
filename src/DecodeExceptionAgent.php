bb0a4836371a5c2cd0bb1161d1a97fd8034c0ef5
Paul Water <p.r.water@setbased.nl>
2020-04-21 08:14:55 +0200

    Add DecodeExceptionAgent.

---
+++ src/DecodeExceptionAgent.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace Plaisio\ExceptionHandler;
+
+use Plaisio\Kernel\Nub;
+use Plaisio\Obfuscator\Exception\DecodeException;
+use Plaisio\Response\BadRequestResponse;
+
+/**
+ * An agent that handles DecodeException exceptions.
+ */
+class DecodeExceptionAgent
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Handles a DecodeException thrown in the constructor of a page object.
+   *
+   * @param DecodeException $exception The exception.
+   *
+   * @since 1.2.0
+   * @api
+   */
+  public function handleConstructException(DecodeException $exception): void
+  {
+    $this->handleException($exception);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Handles a DecodeException thrown during generating the response by a page object.
+   *
+   * @param DecodeException $exception The exception.
+   *
+   * @since 1.2.0
+   * @api
+   */
+  public function handleResponseException(DecodeException $exception): void
+  {
+    $this->handleException($exception);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Handles a DecodeException.
+   *
+   * @param DecodeException $exception The exception.
+   */
+  private function handleException(DecodeException $exception): void
+  {
+    Nub::$DL->rollback();
+
+    // Set the HTTP status to 400 (Bad Request).
+    $response = new BadRequestResponse();
+    $response->send();
+
+    // Log the bad request.
+    Nub::$requestLogger->logRequest($response->getStatus());
+    Nub::$DL->commit();
+
+    // Only on development environment log the error.
+    if (Nub::$request->isEnvDev())
+    {
+      $logger = Nub::$nub->getErrorLogger();
+      $logger->logError($exception);
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
